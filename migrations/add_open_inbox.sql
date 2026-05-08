5039ff6529bfc9c9f47084a96c963979bb087aff
Skeny Antonio <jaciroantonio13@gmail.com>
2026-05-08 20:57:25 +0100

    fix: corrige o add_open_inbox.sql

--- migrations/add_open_inbox.sql
+++ migrations/add_open_inbox.sql
@@ -1,7 +1,7 @@
 -- Adiciona suporte a "caixa de entrada aberta" para contas de suporte
 -- Quando open_inbox = 1, qualquer utilizador autenticado pode enviar mensagem sem ser amigo
 
-ALTER TABLE users ADD COLUMN IF NOT EXISTS open_inbox TINYINT(1) NOT NULL DEFAULT 0;
+ALTER TABLE users ADD COLUMN open_inbox TINYINT(1) NOT NULL DEFAULT 0;
 
 -- Para ativar numa conta de suporte, executar:
 -- UPDATE users SET open_inbox = 1 WHERE username = 'nytubesuport';
