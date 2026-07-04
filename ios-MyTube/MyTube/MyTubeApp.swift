74a87aa6ee1de5617cb5c7a56dabec12715f004c
profissionalantosteve-lab <profissionalantosteve@gmail.com>
2026-07-04 01:53:45 +0100

    chore: adiciona configuração inicial do projeto iOS
    Este commit adiciona a configuração inicial do projeto iOS, estabelecendo a estrutura base necessária para o desenvolvimento da aplicação. Inclui a organização inicial do projeto, configurações essenciais e os arquivos de suporte necessários para permitir a implementação das próximas funcionalidades.
    
    Co-Authored-By: FidelMaluto <fidel.maluto77@gmail.com>

---
+++ ios-MyTube/MyTube/MyTubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTubeApp.swift
+//  MyTube
+//
+//  Created by mendes on 03/07/26.
+//
+
+import SwiftUI
+
+@main
+struct MyTubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios-MyTube/MyTubeTests/MyTubeTests.swift
@@ -0,0 +1,16 @@
+//
+//  MyTubeTests.swift
+//  MyTubeTests
+//
+//  Created by mendes on 03/07/26.
+//
+
+import Testing
+
+struct MyTubeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
