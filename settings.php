020c6e37d48dbc3478831ed54da178c45312dcea
Skeny Antonio <jaciroantonio13@gmail.com>
2026-08-21 14:26:35 +0100

    fix: Actualiza a versao do site

--- settings.php
+++ settings.php
@@ -1041,7 +1041,7 @@ try {
             </div>
         </div>
 
-        <div class="app-version">MyTube v1.10.10</div>
+        <div class="app-version">MyTube v1.10.11</div>
     </main>
 
     <!-- Modal guia Desktop -->
